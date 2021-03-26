4f3e3a9263f6a6a68643afa7a2bdd54c8918be2c
FX <fx.e.morin@gmail.com>
2021-03-26 03:19:47 -0400

    Initializing mod

--- src/main/java/updatesuppressionblock/UpdateSuppressionBlockServer.java
+++ src/main/java/updatesuppressionblock/UpdateSuppressionBlockServer.java
@@ -2,22 +2,25 @@ package updatesuppressionblock;
 
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
+import net.fabricmc.api.ModInitializer;
 
-public class UpdateSuppressionBlockServer implements CarpetExtension
+public class UpdateSuppressionBlockServer implements CarpetExtension, ModInitializer
 {
 
     public static void noop() {}
     static
     {
         CarpetServer.manageExtension(new UpdateSuppressionBlockServer());
-        System.out.print("The mod has loaded!!!!!!!!!!!!!!!!!!");
     }
 
     @Override
     public void onGameStarted()
     {
-        System.out.print("The game has started!!!!!!!!!!!!!!!!!!");
         // let's /carpet handle our few simple settings
         CarpetServer.settingsManager.parseSettingsClass(UpdateSuppressionBlockSettings.class);
+    }
+
+    @Override
+    public void onInitialize() {
     }
 }
