1e5d5299744a82d3bc507e29a7fd34b9b7224696
FX <fx.e.morin@gmail.com>
2021-04-10 22:40:50 -0400

    Made it so that the updateSuppressionBlock can now auto-reset by budding the rail above after the specified amount of ticks.

--- src/main/java/updatesuppressionblock/UpdateSuppressionBlockSettings.java
+++ src/main/java/updatesuppressionblock/UpdateSuppressionBlockSettings.java
@@ -1,14 +1,57 @@
 package updatesuppressionblock;
 
+import carpet.settings.ParsedRule;
 import carpet.settings.Rule;
+import carpet.settings.Validator;
+import net.minecraft.server.command.ServerCommandSource;
 
 import static carpet.settings.RuleCategory.CREATIVE;
 
 public class UpdateSuppressionBlockSettings
 {
+    public static int getInteger(String s) {
+        try {
+            return Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    public static int updateSuppressionBlockSetting = -1;
     @Rule(
             desc = "Placing an activator rail on top of a barrier block will update suppress when the rail turns off.",
-            category = {CREATIVE, "extras"}
+            extra = {"Entering an integer will make the update suppression block auto-reset","Integer entered is the delay in ticks for it to reset"},
+            category = {CREATIVE, "extras"},
+            options = {"false","true","1"},
+            strict = false,
+            validate = updateSuppressionBlockModes.class
     )
-    public static boolean updateSuppressionBlock = false;
+    public static String updateSuppressionBlock = "false";
+
+    //Ghetto work around so I don't need to write my own validator cause im lazy :)
+    private static class updateSuppressionBlockModes extends Validator<String> {
+        @Override
+        public String validate(ServerCommandSource source, ParsedRule<String> currentRule, String newValue, String string) {
+            if (!currentRule.get().equals(newValue)) {
+                if (newValue.equalsIgnoreCase("false")) {
+                    updateSuppressionBlockSetting = -1;
+                } else if (newValue.equalsIgnoreCase("true")) {
+                    updateSuppressionBlockSetting = 0;
+                } else {
+                    int parsedInt = getInteger(newValue);
+                    if (parsedInt <= 0) {
+                        updateSuppressionBlockSetting = -1;
+                        return "false";
+                    } else {
+                        updateSuppressionBlockSetting = parsedInt;
+                    }
+                }
+            }
+            return newValue;
+        }
+        @Override
+        public String description() {
+            return "Cannot be negative, can be true, false, or # > 0";
+        }
+    }
 }
--- src/main/java/updatesuppressionblock/mixins/BarrierBlockMixin.java
+++ src/main/java/updatesuppressionblock/mixins/BarrierBlockMixin.java
@@ -1,30 +1,55 @@
 package updatesuppressionblock.mixins;
 
 import net.minecraft.block.*;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.crash.CrashException;
 import net.minecraft.util.crash.CrashReport;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.TickPriority;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import updatesuppressionblock.UpdateSuppressionBlockSettings;
 
+import java.util.Random;
+
 @Mixin(BarrierBlock.class)
 public class BarrierBlockMixin extends Block {
 
-    public BarrierBlockMixin(Settings settings) {
-        super(settings);
+    private boolean shouldPower = false;
+
+    public BarrierBlockMixin(Settings settings) { super(settings); }
+
+    @Override
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return shouldPower ? 15 : 0;
     }
 
     @Override
     public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
-        if (UpdateSuppressionBlockSettings.updateSuppressionBlock) {
+        if (UpdateSuppressionBlockSettings.updateSuppressionBlockSetting != -1) {
             if (fromPos.equals(pos.up())) {
                 BlockState stateAbove = world.getBlockState(fromPos);
                 if (stateAbove.isOf(Blocks.ACTIVATOR_RAIL) && !stateAbove.get(PoweredRailBlock.POWERED)) {
-                    throw new StackOverflowError();
+                    if (UpdateSuppressionBlockSettings.updateSuppressionBlockSetting > 0) {
+                        world.getBlockTickScheduler().schedule(pos, this, UpdateSuppressionBlockSettings.updateSuppressionBlockSetting);
+                    }
+                    throw new StackOverflowError("updateSuppressionBlock");
                 }
             }
         }
         super.neighborUpdate(state, world, pos, block, fromPos, notify);
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        BlockPos posAbove = pos.up();
+        BlockState stateAbove = world.getBlockState(posAbove);
+        if (stateAbove.isOf(Blocks.ACTIVATOR_RAIL) && !stateAbove.get(PoweredRailBlock.POWERED)) {
+            shouldPower = true;
+            world.setBlockState(posAbove, stateAbove.with(PoweredRailBlock.POWERED, true), 3);
+            shouldPower = false;
+        }
     }
 }
